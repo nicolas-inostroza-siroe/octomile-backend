a20c0b78b9fd20f1ed9488f6d3675487a511cc7f
Ricardo ortega <Ricardo.dao.ort@gmail.com>
2025-02-12 16:02:26 -0300

    fix

--- src/drivers/entities/drivers.entity.ts
+++ src/drivers/entities/drivers.entity.ts
@@ -24,63 +24,63 @@ export class DriversEntity {
     @Column({ type: 'varchar'})
     tipo: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar',nullable: true})
     permiso_circulacion: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     fecha_de_vencimiento_permiso_circulacion: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     revision_tecnica: string;
     
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     fecha_de_vencimiento_revision_tecnica: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable : true})
     soap_al_dia: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar' , nullable: true})
     fecha_de_vencimiento_soap: string;
 
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar' , nullable: true})
     fotografia1: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar' , nullable: true})
     fotografia2: string;
     
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar' , nullable: true})
     fotografia3: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar' , nullable: true})
     fotografia4: string;
 
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     ancho_vehiculo: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar',nullable: true})
     largo_vehiculo: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar',nullable: true})
     alto_vehiculo: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     Carnet_de_identidad_vigente: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     fecha_de_vencimiento_carnet_de_identidad: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     licencia_conductor_vigente: string;
   
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     fecha_de_vencimiento_licencia_conductor: string;
 
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     certificado_anotaciones_vigente: string;
     
-    @Column({ type: 'varchar'})
+    @Column({ type: 'varchar', nullable: true})
     certificado_antecedentes_vigente: string;
 
     @Column('json', { nullable: true })
